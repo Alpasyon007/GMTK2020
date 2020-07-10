3149f7a7e2471c46ef97d6b04dc1351bc82fbb7c
Alpasyon007 <alpasyon07@gmail.com>
2020-07-10 23:51:43 +0100

    INIT

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    /*[SerializeField] float negativeX, positiveX, negativeY, positiveY;*/
+
+    [Range(0, 0.1f)] [SerializeField] float smoothing_factor = 0.2f;
+
+    Vector3 camera_pos;
+    public Vector2 camera_velocity;
+
+    void Awake() {
+        camera_pos = transform.position;
+    }
+
+    private void FixedUpdate() {
+        Vector3 playerPos = new Vector3(player.position.x, player.position.y, -10);
+        Vector3 SmoothedPos = Vector3.Lerp(transform.position, playerPos, smoothing_factor);
+        /*SmoothedPos.x = Mathf.Clamp(SmoothedPos.x, negativeX, positiveX);
+        SmoothedPos.y = Mathf.Clamp(SmoothedPos.y, negativeY, positiveY);*/
+        transform.position =  SmoothedPos;
+
+        camera_velocity = (transform.position - camera_pos) / Time.fixedDeltaTime;
+        camera_pos = transform.position;
+    }
+
+    /*public void UpdateClamp(float new_negative_x, float new_positive_x, float new_negative_y, float new_positive_y) {
+        negativeX = new_negative_x;
+        positiveX = new_positive_x;
+        negativeY = new_negative_y;
+        positiveY = new_positive_y;
+    }*/
+}
---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float speed = 5f;
+    [SerializeField] GameObject bullet;
+    [SerializeField] Camera cam;
+
+    Vector2 mousePos;
+
+
+    void Update() {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetKeyDown(KeyCode.Mouse1)) {
+            Fire();
+        }
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            rb.AddForce(Vector2.up * speed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            rb.AddForce(Vector2.right * speed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            rb.AddForce(Vector2.left * speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            rb.AddForce(Vector2.down * speed);
+        }
+    }
+
+    private void FixedUpdate() {
+        rb.rotation = Mathf.Atan2((mousePos - rb.position).y, (mousePos - rb.position).x) * Mathf.Rad2Deg - 90f;
+    }
+
+    void Fire() {
+        Object.Instantiate(bullet, transform.position, transform.rotation);
+    }
+}
