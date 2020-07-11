62b35c0668ca07ffed9d7623666994e3f0d3f09b
Alpasyon007 <alpasyon07@gmail.com>
2020-07-11 17:33:55 +0100

    Worked more on UI and added a Intro Scene

---
+++ Assets/DialogueManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class DialogueManager : MonoBehaviour {
+
+	public TextMeshProUGUI nameText;
+	public TextMeshProUGUI dialogueText;
+
+	//public Animator animator;
+
+	private Queue<string> sentences;
+
+	// Use this for initialization
+	void Start() {
+		sentences = new Queue<string>();
+	}
+
+	private void Update() {
+		if (Input.GetKeyDown("return")) {
+			DisplayNextSentence();
+		}
+	}
+
+	public void StartDialogue(Dialogue dialogue) {
+		//animator.SetBool("IsOpen", true);
+
+		nameText.text = dialogue.name;
+
+		sentences.Clear();
+
+		foreach (string sentence in dialogue.sentences) {
+			sentences.Enqueue(sentence);
+		}
+		
+		DisplayNextSentence();
+	}
+
+	public void DisplayNextSentence() {
+		if (sentences.Count == 0) {
+			EndDialogue();
+			return;
+		}
+
+		string sentence = sentences.Dequeue();
+		StopAllCoroutines();
+		StartCoroutine(TypeSentence(sentence));
+	}
+
+	IEnumerator TypeSentence(string sentence) {
+		dialogueText.text = "";
+		yield return new WaitForSeconds(0.25f*Time.deltaTime);
+		foreach (char letter in sentence.ToCharArray()) {
+			dialogueText.text += letter;
+			yield return new WaitForSeconds(0.1f*Time.deltaTime);
+		}
+	}
+
+	public void EndDialogue() {
+		SceneManager.LoadScene("Game");
+	}
+
+	//IEnumerator DeleteSentence(string sentence) {
+	//	foreach (char letter in sentence.ToCharArray()) {
+	//		dialogueText.text = dialogueText.text.Replace(letter, '\0');
+	//		yield return new WaitForEndOfFrame();
+	//	}
+	//}
+
+}
---
+++ Assets/DialogueTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    private void Start() {
+        StartCoroutine(StartDialogue());
+    }
+
+    IEnumerator StartDialogue() {
+        yield return new WaitForSeconds(0.1f);
+        gameObject.GetComponent<DialogueManager>().StartDialogue(dialogue);
+    }
+}
